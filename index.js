19913cfd22bc061c9ee4367d2b0250aac183ddbc
Jannis R <mail@jannisr.de>
2020-01-18 10:43:26 +0100

    createClient(): pass through options :bug:, 5.0.1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const createClient = require('hafas-client')
 const dbProfile = require('hafas-client/p/db')
 
-const createHafas = (userAgent) => createClient(dbProfile, userAgent)
+const createHafas = (userAgent, opt = {}) => {
+	return createClient(dbProfile, userAgent, opt)
+}
 
 module.exports = createHafas
